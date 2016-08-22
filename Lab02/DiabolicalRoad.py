616160d3c44c5cea8f0ea27bf241d911235265a4
watcharaphat <watcharaphat.3166@gmail.com>
2016-08-22 11:45:23 +0700

    finished, but count by 1

--- Lab02/DiabolicalRoad.py
+++ Lab02/DiabolicalRoad.py
@@ -1,5 +1,23 @@
 import csv
-f = open('311_Service_Requests_-_Pot_Holes_Reported.csv')
+f = open('311_Service_Requests_-_Pot_Holes_Reported.csv', 'r')
+
+potholes_by_zip = {}
+
 for row in csv.DictReader(f):
-    addr = row['STREET ADDRESS']
+    status = row['STATUS']
+    zipcode = row['ZIP']
     num = row['NUMBER OF POTHOLES FILLED ON BLOCK']
+    if status == 'Open':
+        if zipcode not in potholes_by_zip:
+            potholes_by_zip[zipcode] = 1
+        else:
+            potholes_by_zip[zipcode] += 1
+
+
+print('#\tZIP\tNUM_HOLES')
+
+i = 1
+for key in sorted(potholes_by_zip, key=potholes_by_zip.get, reverse=True):
+    if(i <= 5):
+        print('#' + str(i) + '\t' + key + '\t' + str(potholes_by_zip[key]))
+        i += 1
